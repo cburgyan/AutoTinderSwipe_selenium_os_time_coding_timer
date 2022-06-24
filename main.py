c5eb6b69a8889b52daced7a526c371960c3354ba
cburgyan <cburgyan@gmail.com>
2022-06-23 20:57:29 -0500

    create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import coding_timer
+
+
+coding_timer = coding_timer.CodingTimer("time_main.txt")
+# coding_timer.start_time()
+# coding_timer.pause_time()
+# coding_timer.unpause_time()
+# coding_timer.stop_time()
